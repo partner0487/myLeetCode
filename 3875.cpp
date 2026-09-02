0473c230008cbe6e8702e6e8695f3dc7978e8b0c
Partner0487 <partner0487@gmail.com>
2026-09-02 11:20:07 +0800

    finish 3875. Construct Uniform Parity Array I

---
+++ 3875.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {3, 9, 2, 1, 7};
+    bool ans = sol.uniformArray(nums);
+    cout << ans;
+}
