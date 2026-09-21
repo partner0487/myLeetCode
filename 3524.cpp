d67359a5a804ff77ac83c0e8c380b57ce34e4dbf
partner0487 <partner0487@gmail.com>
2026-09-21 14:05:53 +0800

    finish 3524. Find X Value of Array I

---
+++ 3524.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<long long> resultArray(vector<int> &nums, int k)
+    {
+        vector<long long> dp(k, 0), ans(k, 0);
+        for (auto num : nums)
+        {
+            vector<long long> next_dp(k, 0);
+            int val = num % k;
+            next_dp[val]++;
+            ans[val]++;
+            for (int j = 0; j < k; j++)
+                if (dp[j] > 0)
+                {
+                    next_dp[(j * val) % k] += dp[j];
+                    ans[(j * val) % k] += dp[j];
+                }
+            dp.swap(next_dp);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    Solution sol;
+    vector<int> input = {1, 2, 3, 4, 5};
+    int k = 3;
+
+    vector<long long> output = sol.resultArray(input, k);
+    for (auto i : output)
+    {
+        cout << i << endl;
+    }
+}
